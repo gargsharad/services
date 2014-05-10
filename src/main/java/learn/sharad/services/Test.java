d1cfbb096984878bec03d93dcf1f31c4436d4c50
gargsharad <garg.sharadkumar@gmail.com>
2014-05-10 00:03:42 -0700

    test

--- src/main/java/learn/sharad/services/Test.java
+++ src/main/java/learn/sharad/services/Test.java
@@ -1,5 +1,36 @@
 package learn.sharad.services;
 
 public class Test {
+	StringBuilder sb = new StringBuilder("JAN");
 
+	public static void main(String[] args) {
+		/*int[] arr = new int[5];
+		byte b = 4;
+		char c = 'c';
+		long longVar = 10;
+		arr[0] = b;
+		arr[1] = c;
+		int[] arr1;
+		int[] arr2 = new int[3];
+		char[] arr3 = { 'a', 'b' };
+		arr1 = arr2;
+		// arr1 = arr3;
+		System.out.println(arr1[0] + ":" + arr1[1]);
+		StringBuilder ejg = new StringBuilder(10 + 2 + "SUN" + 4 + 5);
+		ejg.append(ejg.delete(3, 6));
+		System.out.println(ejg);*/
+		
+		String name = "Lion";
+		int score = 100;
+		if (name.equals("Lion"))
+		score = 200;
+		name = "Larry"; 
+		//else 
+		score = 129;
+		
+		boolean allow = false;
+		if (allow = true) 
+		System.out.println("value is true");
+		
+	}
 }
